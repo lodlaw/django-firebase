6a2942b64eadd083f6d6e121da7be274c1febd81
Anh Pham <anhphamduy@outlook.com>
2021-11-16 13:59:25 +1100

    Update initialize method

--- django_firebase/initialize.py
+++ django_firebase/initialize.py
@@ -1,4 +1,3 @@
-import json
 import os
 
 import firebase_admin
@@ -11,27 +10,29 @@ def make_service_account_envar_name(name):
 
 
 def initialize():
-    config = {
-        'type': os.environ[make_service_account_envar_name('TYPE')],
-        'project_id': os.environ[make_service_account_envar_name('PROJECT_ID')],
-        'private_key_id': os.environ[make_service_account_envar_name('PRIVATE_KEY_ID')],
-        'private_key': os.environ[make_service_account_envar_name('PRIVATE_KEY')],
-        'client_email': os.environ[make_service_account_envar_name('CLIENT_EMAIL')],
-        'client_id': os.environ[make_service_account_envar_name('CLIENT_ID')],
-        'auth_uri': os.environ[make_service_account_envar_name('AUTH_URI')],
-        'token_uri': os.environ[make_service_account_envar_name('TOKEN_URI')],
-        'auth_provider_x509_cert_url': os.environ[make_service_account_envar_name('AUTH_PROVIDER_X509_CERT_URL')],
-        'client_x509_cert_url': os.environ[make_service_account_envar_name('CLIENT_X509_CERT_URL')],
-    }
+    config = f"""
+    {{
+    "type": "{os.environ[make_service_account_envar_name('TYPE')]}",
+        "project_id": "{os.environ[make_service_account_envar_name('PROJECT_ID')]}",
+        "private_key_id": "{os.environ[make_service_account_envar_name('PRIVATE_KEY_ID')]}",
+        "private_key": "{os.environ[make_service_account_envar_name('PRIVATE_KEY')]}",
+        "client_email": "{os.environ[make_service_account_envar_name('CLIENT_EMAIL')]}",
+        "client_id": "{os.environ[make_service_account_envar_name('CLIENT_ID')]}",
+        "auth_uri": "{os.environ[make_service_account_envar_name('AUTH_URI')]}",
+        "token_uri": "{os.environ[make_service_account_envar_name('TOKEN_URI')]}",
+        "auth_provider_x509_cert_url": "{os.environ[make_service_account_envar_name('AUTH_PROVIDER_X509_CERT_URL')]}",
+        "client_x509_cert_url": "{os.environ[make_service_account_envar_name('CLIENT_X509_CERT_URL')]}"
+    }}
+    """
 
     path = './cred.json'
     try:
-        with open(path, 'a') as f:
-            json.dump(config, f)
-
+        with open(path, 'w') as f:
+            f.write(config)
+            # json.dump(config, f)
         os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = path
 
         if not firebase_admin._apps:
             firebase_admin.initialize_app()
     finally:
-        os.remove(path)
+        pass
